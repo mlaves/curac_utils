e2dcb6113e86182a4134ab1b1ce274b889329121
mlaves <max-heinrich@mhlaves.de>
2020-04-13 18:11:01 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -14,6 +14,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mlaves/curac_utils",
     packages=setuptools.find_packages(),
+    install_requires=[
+              'numpy',
+              'torch',
+              'progressbar',
+              'tqdm',
+              'urllib'
+          ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
