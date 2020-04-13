062169412ce1ccbae15bb37a8bcf0045b60f6386
mlaves <max-heinrich@mhlaves.de>
2020-04-13 21:40:28 +0200

    fixed setup.py

--- curac_utils/__init__.py
+++ curac_utils/__init__.py
@@ -3,7 +3,7 @@
 Package docstring.
 """
 
-from ._version import __version__
 from ._lung_ct_dataset import LungCTDataset
 
+__version__ = '0.0.6'
 __all__ = ['LungCTDataset']
--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+import codecs
+import os.path
 import setuptools
-from curac_utils._version import __version__
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="curac_utils-pkg-max0r",
-    version=__version__,
+    version=get_version("curac_utils/__init__.py"),
     author="Max-Heinrich Laves",
     author_email="author@example.com",
     description="A helper package for CURAC lecture.",
@@ -14,7 +31,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mlaves/curac_utils",
     packages=setuptools.find_packages(),
-    setup_requires=[
+    install_requires=[
               'numpy>=1.16.4',
               'torch>=1.4.0',
               'progressbar>=2.5',
