d9ebe5b6921ed0e596eb29e60f0cb015abe99113
mlaves <max-heinrich@mhlaves.de>
2020-04-14 15:25:47 +0200

    added first task

--- curac_utils/__init__.py
+++ curac_utils/__init__.py
@@ -4,6 +4,7 @@ Package docstring.
 """
 
 from ._lung_ct_dataset import LungCTDataset
+from ._uebung_z import UebungZ
 
-__version__ = '0.0.6'
-__all__ = ['LungCTDataset']
+__version__ = '0.0.8'
+__all__ = ['LungCTDataset', 'UebungZ']
---
+++ curac_utils/_uebung_z.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class UebungZ():
+    
+    @staticmethod
+    def aufgabe_1():
+        """
+        Diese Funktion gibt das Ergebnis der Aufgabe 1 aus.
+        """
+        print("Herzlichen Glückwunsch! Du hast Aufgabe 1 der Zusatzübung erfolgreich abgeschlossen.")
