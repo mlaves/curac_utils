989db7606a4a96abdfb4ba335f3352e7d79a9344
mlaves <max-heinrich@mhlaves.de>
2020-05-05 13:17:51 +0200

    added next excercise

--- curac_utils/__init__.py
+++ curac_utils/__init__.py
@@ -6,10 +6,12 @@ Package docstring.
 from ._version import __version__
 from ._uebung_0 import Uebung_0
 from ._uebung_1 import Uebung_1
+from ._uebung_2 import Uebung_2
 #from ._lung_ct_dataset import LungCTDataset
 
 __all__ = [
     #'LungCTDataset',
     'Uebung_0',
-    'Uebung_1'
+    'Uebung_1',
+    'Uebung_2'
 ]
